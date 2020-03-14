fa04df0fd75ba112fbd9312c887d484577b16864
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-03-14 16:12:38 +0200

    feat: added seeds for stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,11 +1,16 @@
 <template>
   <section>
     <h2>Your Stats</h2>
+    <p v-html="data"></p>
   </section>
 </template>
 
 <script>
+import { data } from "@/seed.js";
 export default {
-  name: "Stats"
+  name: "Stats",
+  data: () => ({
+    data
+  })
 };
 </script>
---
+++ src/seed.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    age: 42,
+    city: "Port Said",
+    country: "Egypt",
+    latitude: "31.237755086533454",
+    longitude: "32.31979524546545",
+    gender: "male",
+    ip_address: "192.448.394.11",
+    status: "recovered"
+  },
+  {
+    age: 42,
+    city: "Port Said",
+    country: "Egypt",
+    latitude: "31.237755086533454",
+    longitude: "32.31979524546545",
+    gender: "male",
+    ip_address: "192.448.394.11",
+    status: "recovered"
+  },
+  {
+    age: 42,
+    city: "Port Said",
+    country: "Egypt",
+    latitude: "31.237755086533454",
+    longitude: "32.31979524546545",
+    gender: "male",
+    ip_address: "192.448.394.11",
+    status: "recovered"
+  },
+  {
+    age: 42,
+    city: "Port Said",
+    country: "Egypt",
+    latitude: "31.237755086533454",
+    longitude: "32.31979524546545",
+    gender: "male",
+    ip_address: "192.448.394.11",
+    status: "recovered"
+  },
+  {
+    age: 42,
+    city: "Port Said",
+    country: "Egypt",
+    latitude: "31.237755086533454",
+    longitude: "32.31979524546545",
+    gender: "male",
+    ip_address: "192.448.394.11",
+    status: "recovered"
+  }
+];
